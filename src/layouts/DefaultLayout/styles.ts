2631759dca43d47df3117dddf205a1efbb29bbc4
Flávio Renato <peubevbr@gmail.com>
2023-01-22 12:07:28 -0300

    Inicializando o projeto Timer utilizando Reactjs

---
+++ src/layouts/DefaultLayout/styles.ts
@@ -0,0 +1,16 @@
+import styled from 'styled-components'
+
+export const LayoutContainer = styled.div`
+  max-width: 74rem;
+  height: calc(
+    100vh - 10rem
+  ); /* -10rem => 5rem de margim em cima e 5rem em baixo*/
+  margin: 5rem auto; /* auto => fica centralizado */
+  padding: 2.5rem;
+
+  background-color: ${(props) => props.theme['gray-800']};
+  border-radius: 8px;
+
+  display: flex;
+  flex-direction: column;
+`

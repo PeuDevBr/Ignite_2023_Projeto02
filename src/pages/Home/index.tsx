6a8d70d60c4830c80844542c6ec78676c875d815
Flávio Renato <peubevbr@gmail.com>
2023-01-23 20:06:26 -0300

    Adicionando a biblioteca hookform e validação

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,6 +1,9 @@
 import { Play } from 'phosphor-react'
 import { useForm } from 'react-hook-form'
 
+import { zodResolver } from '@hookform/resolvers/zod'
+import * as zod from 'zod'
+
 import {
   HomeContainer,
   FormContainer,
@@ -11,8 +14,18 @@ import {
   MinutesAmountInput,
 } from './styles'
 
+const newCycleFormValidationSchema = zod.object({
+  task: zod.string().min(1, 'Informe a tarefa '),
+  minutesAmount: zod
+    .number()
+    .min(5, 'O ciclor precisa ser no mínimo 5 minutos')
+    .max(60, 'O ciclor precisa ser no máximo 60 minutos'),
+})
+
 export function Home() {
-  const { register, handleSubmit, watch } = useForm()
+  const { register, handleSubmit, watch } = useForm({
+    resolver: zodResolver(newCycleFormValidationSchema),
+  })
 
   function handleCreateNewCycle(data: any) {
     console.log(data)
